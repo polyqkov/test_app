0fa2d3c3b77c35d0ab7ecb1dbb7e022f6a193a0b
alemmiwi <yellowfirehydrant@icloud.com>
2022-10-11 00:52:30 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(home: Container());
+}
